ba27991938043d60a40ee251d5b551bdf30e8e48
delee <de.lee@lawcompany.co.kr>
2018-06-27 10:25:15 +0900

    add integration-test, npm-shrinkwrap

---
+++ movies-service/integration-test/index.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest')
+
+describe('movies-service', () => {
+
+  const api = supertest('http://localhost:3000')
+
+  it('returns a 200 for a collection of movies', (done) => {
+
+    api.get('/movies/premiers')
+      .expect(200, done)
+  })
+})
